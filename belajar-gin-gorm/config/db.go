e3080016ab91d0fbf0b483d9f11376f0c1336f72
divfadli <fadlilahdivyy@gmail.com>
2025-05-06 19:09:46 +0700

    coba-railways

---
+++ belajar-gin-gorm/config/db.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"api-gin/models"
+	"api-gin/utils"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectDataBase() *gorm.DB {
+	username := utils.Getenv("DATABASE_USERNAME", "root")
+	password := utils.Getenv("DATABASE_PASSWORD", "password")
+	host := utils.Getenv("DATABASE_HOST", "127.0.0.1")
+	port := utils.Getenv("DATABASE_PORT", "3306")
+	database := utils.Getenv("DATABASE_NAME", "database_movie")
+
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.AutoMigrate(&models.User{}, &models.Movie{}, &models.AgeRatingCategory{})
+
+	return db
+}
---
+++ belajar-gin-gorm/routes/routes.go
@@ -0,0 +1,52 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	"belajar-gorm-gin/controllers"
+	"belajar-gorm-gin/middlewares"
+
+	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+)
+
+func SetupRouter(db *gorm.DB) *gin.Engine {
+	r := gin.Default()
+
+	// set db to gin context
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+	})
+
+	r.POST("/register", controllers.Register)
+	r.POST("/login", controllers.Login)
+
+	// r.GET("/movies", controllers.GetAllMovie)
+	// r.GET("/:id", controllers.GetMovieById)
+
+	moviesMiddlewareRoute := r.Group("/movies")
+	moviesMiddlewareRoute.Use(middlewares.JwtAuthMiddleware())
+	moviesMiddlewareRoute.GET("/movies", controllers.GetAllMovie)
+	moviesMiddlewareRoute.GET("/:id", controllers.GetMovieById)
+	moviesMiddlewareRoute.POST("/", controllers.CreateMovie)
+	moviesMiddlewareRoute.PATCH("/:id", controllers.UpdateMovie)
+	moviesMiddlewareRoute.DELETE("/:id", controllers.DeleteMovie)
+
+	// r.GET("/age-rating-categories", controllers.GetAllRating)
+	// r.GET("/age-rating-categories/:id", controllers.GetRatingById)
+	// r.GET("/age-rating-categories/:id/movies", controllers.GetMoviesByRatingId)
+
+	ratingMiddlewareRoute := r.Group("/age-rating-categories")
+	ratingMiddlewareRoute.Use(middlewares.JwtAuthMiddleware())
+	ratingMiddlewareRoute.GET("", controllers.GetAllRating)
+	ratingMiddlewareRoute.GET("/:id", controllers.GetRatingById)
+	ratingMiddlewareRoute.GET("/:id/movies", controllers.GetMoviesByRatingId)
+	ratingMiddlewareRoute.POST("/", controllers.CreateRating)
+	ratingMiddlewareRoute.PATCH("/:id", controllers.UpdateRating)
+	ratingMiddlewareRoute.DELETE("/:id", controllers.DeleteRating)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	return r
+}
